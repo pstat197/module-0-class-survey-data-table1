f85bcf27bde55731e0ec7c774e07561e0e2beac1
anjanisethi <anjsethi3@gmail.com>
2025-10-21 23:37:11 +0200

    Update anjani_assignment1.R
    splitting data based on research experience + descriptive statistics

--- scripts/anjani_assignment1.R
+++ scripts/anjani_assignment1.R
@@ -1,6 +1,5 @@
 # do students with research experience (rsrch = TRUE) tend to
 ## be more comfortable with programming?
-### 'comfortable' means 4 <= prog.comf <= 5
 
 library(readr)
 library(dplyr)
@@ -13,3 +12,28 @@ interest <- read_csv('data/interest-clean.csv')
 merge_df <- merge(background, interest, by='response_id')
 
 view(merge_df)
+
+rsrch_yes <- merge_df %>% 
+  filter(rsrch == TRUE) %>% 
+  select(prog.comf)
+
+view(rsrch_yes)
+
+rsrch_no <- merge_df %>% 
+  filter(rsrch == FALSE) %>% 
+  select(prog.comf)
+
+view(rsrch_no)
+
+# defining our two distinct populations: those with research and
+## those without research
+
+rsrch_yes %>% 
+  summarize(prop.prog.comf = mean(prog.comf),
+            med.comf = median(prog.comf))
+
+rsrch_no %>% 
+  summarize(prop.prog.comf = mean(prog.comf),
+            med.comf = median(prog.comf))
+
+# numerical comparisons for comfortability
