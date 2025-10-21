af600828849aba11655a22812b0b7696236677ec
anjanisethi <anjsethi3@gmail.com>
2025-10-21 23:42:19 +0200

    Update anjani_assignment1.R
    new variable to measure comfort w/ programming

--- scripts/anjani_assignment1.R
+++ scripts/anjani_assignment1.R
@@ -1,5 +1,6 @@
 # do students with research experience (rsrch = TRUE) tend to
 ## be more comfortable with programming?
+### 'comfortable' means 4 <= prog.comf <= 5
 
 library(readr)
 library(dplyr)
@@ -36,4 +37,16 @@ rsrch_no %>%
   summarize(prop.prog.comf = mean(prog.comf),
             med.comf = median(prog.comf))
 
-# numerical comparisons for comfortability
+# numerical comparisons for comfort with programming
+
+new_merge_df <- merge_df %>% 
+  mutate(comfortable = ifelse(prog.comf >= 4, TRUE, FALSE))
+
+new_merge_df %>% 
+  group_by(rsrch) %>% 
+  summarize(prop_comfortable = mean(comfortable))
+
+# gives us a proportion of students in each group feel comfortable
+## with programming
+
+
